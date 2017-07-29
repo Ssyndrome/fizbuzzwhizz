86a3002373a91a2e6f517f0c1689bf5eb5df1159
915448715@qq.com <915448715@qq.com>
2017-07-29 17:20:00 +0800

    add main.js & test.js

--- src/main/main.js
+++ src/main/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
-function fizbuzzwhizz(a, b, c){
-  // write your code here...
+function fizbuzzwhizz(a, b, c, number){
+    if(number.toString().indexOf('3')!==-1){
+        return "fizz";
+    }else if(number%a===0){
+        if(number%b===0){
+            if(number%c===0){
+                return `${a},${b},${c},fizzbuzzwhizz`;
+            }else{
+                return `${a},${b},fizzbuzz`;
+            }
+        }else if(number%c===0){
+            return `${a},${c},fizzwhizz`;
+        }else{
+            return "fizz";
+        }
+    }else if(number%b===0){
+        if(number%c===0){
+            return `${b},${c},buzzwhizz`;
+        }else{
+            return "buzz";
+        }
+    }else if(number%c===0){
+        return "whizz";
+    }
 }
--- src/test/main-test.js
+++ src/test/main-test.js
@@ -1,9 +1,71 @@
 'use strict';
 
 describe('fizbuzzwhizz', () => {
+    it('should return fizz when number is mutiple of a', () => {
+        let a = 3;
+        let b = 5;
+        let c = 7;
+        let number = 51;
+        expect(fizbuzzwhizz(a, b, c, number)).toEqual("fizz");
+    });
 
-  // write your tests here...
-  it('should do something when some condition', () => {
-    expect(false).toEqual(true);
-  });
+    it('should return buzz when number is mutiple of b', () => {
+        let a = 3;
+        let b = 5;
+        let c = 7;
+        let number = 40;
+        expect(fizbuzzwhizz(a, b, c, number)).toEqual("buzz");
+    });
+
+    it('should return whizz when number is mutiple of c', () => {
+        let a = 3;
+        let b = 5;
+        let c = 7;
+        let number = 56;
+        expect(fizbuzzwhizz(a, b, c, number)).toEqual("whizz");
+    });
+
+    it('should return number&fizzbuzz when number is mutiple of a&b', () => {
+        let a = 3;
+        let b = 5;
+        let c = 7;
+        let number = 15;
+        let result = `${a},${b},fizzbuzz`;
+        expect(fizbuzzwhizz(a, b, c, number)).toEqual(result);
+    });
+
+it('should return number&fizzwhizz when number is mutiple of a&c', () => {
+    let a = 3;
+let b = 5;
+let c = 7;
+let number = 21;
+let result = `${a},${c},fizzwhizz`;
+expect(fizbuzzwhizz(a, b, c, number)).toEqual(result);
+});
+
+it('should return number&buzzwhizz when number is mutiple of b&c', () => {
+    let a = 3;
+let b = 5;
+let c = 7;
+let number = 70;
+let result = `${b},${c},buzzwhizz`;
+expect(fizbuzzwhizz(a, b, c, number)).toEqual(result);
+});
+
+it('should return number&fizzbuzzwhizz when number is mutiple of a&b&c', () => {
+    let a = 3;
+let b = 5;
+let c = 7;
+let number =105;
+let result = `${a},${b},${c},fizzbuzzwhizz`;
+expect(fizbuzzwhizz(a, b, c, number)).toEqual(result);
+});
+
+it('should return fizz when number including a', () => {
+    let a = 3;
+let b = 5;
+let c = 7;
+let number =13;
+expect(fizbuzzwhizz(a, b, c, number)).toEqual("fizz");
+});
 });
